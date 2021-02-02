c4145feef0b7655dec8e845a13842f8d261fd6df
River Smith <rivyyriv@gmail.com>
2021-02-02 16:40:17 -0700

    App + Login + Create

---
+++ my-app/src/components/login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth"
+
+
+const Login = (props) => {
+
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+    phonenumber: "",
+
+  });
+
+  const handleChange = (e) => {
+
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const login =e=>{
+    e.preventDefault();
+    setCredentials({username:"",password:"",phonenumber: "",})
+
+   axiosWithAuth()
+   .post('/login',credentials)
+    .then(res =>{
+      window.localStorage.setItem('token',res.data.payload)
+      props.history.push("/protected")
+    })
+
+  }
+
+
+  return (
+    <div className="login-card">
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+          <input
+          type="text"
+          name="phonenumber"
+          value={credentials.phonenumber}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login; 
